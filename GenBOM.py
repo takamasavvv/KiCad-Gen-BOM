b08b32b938fd0df21acaf53d1da33acb781bd4e0
takamasavvv <hiratsukatakamasa@gmail.com>
2018-12-11 20:57:34 +0900

    partからformatへの出力方法編集中

--- GenBOM.py
+++ GenBOM.py
@@ -3,74 +3,58 @@ import re
 import sys
 import csv
 
-kc_pjname = "STR-X-Avionics"
+import json
+from collections import OrderedDict
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
-kc_xmldir = "./" + kc_pjname + "/" + kc_pjname + ".xml"
+
 component = ".//comp"
 
 desig_ptn = re.compile(r'[a-zA-Z][a-zA-Z]?')
-field_name = ['Desig', 'MPN', 'Agency']
-header_name = ['Designator', 'Manufacturer Part Number', 'Quantity', 'Agency']
 
+def define_config():
+    """ """
+    with open("./conf.json", 'r') as conf:
+        config = json.load(conf)[0]
+        config['project']['bom_format'] = \
+            config['BOM_formats'][config['project']['bom_format']]
+        return config['project']
 
-def combine(fd_key, fd_value):
-    """  """
-    field_dict = {}
-    field_dict[field_name[0]] = fd_key
-    field_dict.update(fd_value)
-    return field_dict
+def extract_parts(xml_dir):
+    """ """
+    parts_list = []
+    tree = ET.parse(xml_dir)
+    root = tree.getroot()
+    for comp in root.iter('comp'):
+        part = {}
+        part['ref'] = comp.attrib['ref']
+        for field in comp.iter('field'):
+            if field.text in 'value':
+                part[field.attrib['name']] = None
+            else:
+                part[field.attrib['name']] = field.text
+        parts_list.append(part)
+    return parts_list
 
-
-def keyfunc(desig):
-    return int(desig_ptn.split(desig)[1])
-
+def bom_list_gen(parts_list, bom_format):
+    """ """
+    bom_list = []
+    for part in parts_list:
+        bom_line = bom_format
+        if part['MPN'] not in bom_list[0:-1]['Manufacturer Part Number']:
+            print("new")
+        else:
+            print("old")
 
 def main():
     """  """
-    tree = ET.parse(kc_xmldir)
-    root = tree.getroot()
-    rows = {}
+    project = define_config()
+    xml_dir = project['dir'] + project['name'] + "/" + project['name'] + ".xml"
+    
+    bom_list_gen(extract_parts(xml_dir), project['bom_format'])
+    
 
-    field_extract = [
-        combine(desig_raw.get("ref"), {
-            field.get('name'): field.text
-            for field in root.findall(
-                component + "[@ref=\'" + desig_raw.get("ref") + "\']/fields/*"
-            )
-            if field.get('name') in field_name
-        })
-        for desig_raw in root.findall(component)
-    ]
-    for field_fact in field_extract:
-        if field_fact[field_name[1]] not in rows:
-            rows[field_fact[field_name[1]]] = {
-                'Designator': [],
-                'Manufacturer Part Number': None,
-                'Quantity': None,
-                'Agency': None
-            }
-        rows[field_fact[field_name[1]]][
-            header_name[0]].append(field_fact[field_name[0]])
-        rows[field_fact[field_name[1]]][
-            header_name[1]] = field_fact[field_name[1]]
-        rows[field_fact[field_name[1]]][
-            header_name[2]] = len(rows[field_fact[field_name[1]]][header_name[0]])
-        rows[field_fact[field_name[1]]][
-            header_name[3]] = field_fact[field_name[2]]
-    for npm, ls in rows.items():
-        rows[npm][header_name[0]] = ','.join(
-            sorted(rows[npm][header_name[0]],key=lambda x:keyfunc(x))
-        )
-    with open('test.csv', 'w', newline='\n') as ds:
-        writer = csv.DictWriter(ds, header_name)
-        writer.writeheader()
-        for row in rows.values():
-            # print(row['Agency'])
-            if row['Agency'] == 'Digikey' or 'Mouser':
-                # print("OK")
-                writer.writerow(row)
-            else:
-                print("NG")
 
 if __name__ == '__main__':
     main()
